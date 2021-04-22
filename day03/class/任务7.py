19dc17280ed4d6b63a78dab001a0c90d8253e6fb
yangtiankai <yangtk@tiens.com>
2021-04-22 16:14:46 +0800

    修改任务7的错误

--- day03/class/任务7.py
+++ day03/class/任务7.py
@@ -6,12 +6,12 @@
 num = 1
 sum1 = 0
 
-while num <= 100:
+while num < 101:
 
     sum1 = sum1 + num
     num = num+1
 
-    if num == 100:
+    if num > 100:
         print("1~100以内的数的和！为：",sum1)
 
 
